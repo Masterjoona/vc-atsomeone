9879ce9c32140df8a4569fdc895f500e6dfe835b
Masterjoona <69722179+Masterjoona@users.noreply.github.com>
2024-08-03 14:38:31 +0000

    add random person from role

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ Mention someone randomly
 
 Due to some limitations, it won't show on autocomplete in dms/groups but it still works.
 
+Do `@role*` to mention a random person with that role.
+
 ![image](https://github.com/user-attachments/assets/35538b5c-2bfa-4184-b1be-6ffb6a59a2c6)
 ![image](https://github.com/user-attachments/assets/f92aa58f-3573-4f08-89e3-1356d01c1905)
--- index.ts
+++ index.ts
@@ -33,21 +33,28 @@ export default definePlugin({
     ],
     start() {
         this.preSend = addPreSendListener((_, msg) => {
-            msg.content = msg.content.replace(/@someone/g, ()=>`<@${randomUser()}>`);
+            msg.content = msg.content.replace(/@someone/g, ()=>`<@${this.randomUser()}>`);
+            msg.content = msg.content.replace(/<@&(\d+)>\*/g, (_, roleId) => {
+                return `<@${this.randomUser(roleId)}>`;
+            });
         });
     },
 
     stop() {
         removePreSendListener(this.preSend);
+    },
+
+    randomUser(roleId: string = ""): string {
+        const guildId = SelectedGuildStore.getGuildId();
+        if (guildId === null) {
+            const dmUsers = ChannelStore.getChannel(SelectedChannelStore.getChannelId()).recipients;
+            return dmUsers[~~(dmUsers.length * Math.random())];
+        }
+        let members = GuildMemberStore.getMembers(guildId);
+        if (roleId) {
+            members = members.filter(m => m.roles.includes(roleId));
+        }
+        console.log(members);
+        return members[~~(members.length * Math.random())].userId;
     }
 });
-
-const randomUser = () => {
-    const guildId = SelectedGuildStore.getGuildId();
-    if (guildId === null) {
-        const dmUsers = ChannelStore.getChannel(SelectedChannelStore.getChannelId()).recipients;
-        return dmUsers[~~(dmUsers.length * Math.random())];
-    }
-    const members = GuildMemberStore.getMembers(guildId);
-    return members[~~(members.length * Math.random())].userId;
-};
